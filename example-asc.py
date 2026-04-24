c6f6e96f893ceea5d63b283b17efea34ed398b46
Arne Naegel <naegel@gcsc.uni-frankfurt.de>
2026-04-24 13:20:39 +0200

    Update for ASC raster (but still failing)

--- example-asc.py
+++ example-asc.py
@@ -16,33 +16,41 @@ import modsimtools as util
 # We read a grid & create a grid function (for storing data)
 filename="grids/square.ugx" # "square", "corner-mixed"
 dom = util.CreateDomain(filename, 6, "")
-approxSpaceDesc = dict(fct = "u", type = "Lagrange", order = 1)
-approxSpace = util.CreateApproximationSpace(dom, approxSpaceDesc)
+approxSpace = util.CreateApproximationSpace(dom, dict(fct = "u", type = "Lagrange", order = 1))
 ufield = ug4.GridFunction2dCPU1(approxSpace)
 
 
 ############################################
 # Read ESRI-ASC
 ############################################
-if True:
-    try:
-        myRaster = ug4.NumberRaster2d()
-        myRaster.load_from_asc("images/example.asc")
-        # myRaster.blur(desc.blurAlpha, desc.blurIterations)
-    except Exception as inst:
-        print(type(inst))    # the exception type
-        print(inst.args)     # arguments stored in .args
-        print(inst)          #
 
-    try:
-        rasterNumberData = ug4.RasterNumberData2d(myRaster)
-        rasterNumberData.set_order(2)
-        rasterNumberData.set_scale(1.0)
-    except Exception as inst:
-        print(type(inst))    # the exception type
-        print(inst.args)     # arguments stored in .args
-        print(inst)          #
+try:
+    myRaster = ug4.NumberRaster2d()
+    myRaster.load_from_asc("images/example.asc")
+    # myRaster.blur(desc.blurAlpha, desc.blurIterations)
+except Exception as inst:
+    print ("Error loading raster from ASC:")
+    print(type(inst))    # the exception type
+    print(inst.args)     # arguments stored in .args
+    print(inst)          #
 
-    # Write data.
+try:
+    rasterNumberData = ug4.RasterNumberData2d(myRaster)
+    rasterNumberData.set_order(2)
+    rasterNumberData.set_scale(1.0)
+except Exception as inst:
+    print ("Error creating RasterNumberData2d:")
+    print(type(inst))    # the exception type
+    print(inst.args)     # arguments stored in .args
+    print(inst)          #
+
+try:
+    # Interpolate the raster data onto the finite element grid,
+    # write to VTK for visualization.
     ug4.Interpolate(rasterNumberData, ufield, "u")
     ug4.WriteGridFunctionToVTK(ufield, f"vtk/Field_RasterASC")
+except Exception as inst:
+    print ("Error writing grid function to VTK:")
+    print(type(inst))    # the exception type
+    print(inst.args)     # arguments stored in .args
+    print(inst)     
